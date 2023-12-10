8cdcaf0c5c645041a694ffd7c426623e7961e5ec
Matthew <mchen146@ucsc.edu>
2023-12-09 23:50:07 -0800

    added driver register functionality

--- scripts/odbc_driver.py
+++ scripts/odbc_driver.py
@@ -1,4 +1,4 @@
-import winreg, argparse, json
+import os, winreg, argparse
 
 ODBCINST = "SOFTWARE\\ODBC\\ODBCINST.INI"
 
@@ -17,14 +17,27 @@ def main():
 
     path_parser = sub_parsers.add_parser("path", help="get driver path")
     path_parser.add_argument(
-        "driver_name",
+        "name",
         type=str,
         help="name of the driver"
+    )
+
+    register_parser = sub_parsers.add_parser("register", help="register a driver in ODBCINST")
+    register_parser.add_argument(
+        "name",
+        type=str,
+        help="name of the driver"
+    )
+    register_parser.add_argument(
+        "path",
+        type=str,
+        help="path to the driver"
     )
 
     args = arg_parser.parse_args()
 
     if args.cmd == 'ls':
+        import json
         with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, ODBCINST) as odbc_key:
             odbc_info = winreg.QueryInfoKey(odbc_key)
             drivers = dict()
@@ -44,7 +57,7 @@ def main():
         else:
             print(json.dumps(list(drivers.keys()), indent=2))
     elif args.cmd == 'path':
-        driver_name: str = args.driver_name
+        driver_name: str = args.name
         with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, ODBCINST) as odbc_key:
             try:
                 with winreg.OpenKey(odbc_key, driver_name) as driver_key:
@@ -53,6 +66,26 @@ def main():
             except OSError as e:
                 print(f"failed to find driver \"{driver_name}\"")
                 exit(1)
+    elif args.cmd == 'register':
+        import ctypes
+        if ctypes.windll.shell32.IsUserAnAdmin() == 0:
+            print(f"admin privileges required to register a driver")
+            exit(1)
+        driver_name: str = args.name
+        driver_path: str = args.path
+        if not os.path.exists(driver_path):
+            print(f"path \"{driver_path}\" does not exist")
+            exit(1)
+        with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, ODBCINST, 0, winreg.KEY_CREATE_SUB_KEY) as odbc_key:
+            try:
+                with winreg.OpenKey(odbc_key, driver_name) as driver_key:
+                    print(f"driver \"{driver_name}\" already registered")
+                    exit(1)
+            except OSError as e:    
+                with winreg.CreateKey(odbc_key, driver_name) as driver_key:
+                    winreg.SetValueEx(driver_key, "Driver", 0, winreg.REG_SZ, driver_path)
+                    winreg.SetValueEx(driver_key, "Setup", 0, winreg.REG_SZ, driver_path)
+                print(f"successfully registered \"{driver_name}\"")
 
 if __name__ == "__main__":
     main()
